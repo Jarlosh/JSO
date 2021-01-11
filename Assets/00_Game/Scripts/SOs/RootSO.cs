6e417da48acbe6e208cddf70e6348ae0aa1f4471
Jarl <Jarlo@bk.ru>
2021-01-11 13:52:58 +0500

    Added test assets

---
+++ Assets/00_Game/Scripts/SOs/RootSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _00_Game.Scripts.SOs.SOs
+{
+    [CreateAssetMenu(fileName = "RootSO", menuName = "SO/RootSO", order = 0)]
+    public class RootSO : ScriptableObject
+    {
+        public int rootInt = 1;
+        public RootSO root;
+    }
+}
---
+++ Assets/00_Game/Scripts/TestBeh.cs
@@ -0,0 +1,10 @@
+﻿using _00_Game.Scripts.SOs.SOs;
+using UnityEngine;
+
+namespace _00_Game.Scripts.SOs
+{
+    public class TestBeh : MonoBehaviour
+    {
+        [SerializeField] private RootSO root;
+    }
+}
